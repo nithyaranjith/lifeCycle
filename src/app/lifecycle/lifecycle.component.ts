52425d48d0470aba706df3ea839ffb97441a354d
nithyaranjith <nithyamurugan7565@gmail.com>
2024-01-27 21:19:21 +0530

    ng-content

---
+++ src/app/lifecycle/lifecycle.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lifecycle',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './lifecycle.component.html',
+  styleUrl: './lifecycle.component.css'
+})
+export class LifecycleComponent {
+
+}
